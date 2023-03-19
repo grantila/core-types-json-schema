958668a2b708962f5d30f77d645376a5a0cef76d
Gustaf Räntilä <g.rantila@gmail.com>
2023-03-19 10:57:23 +0100

    feat(deps): Bumped core-types and openapi-json-schema

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-	resolver: 'ts-jest-resolver',
-	testEnvironment: 'node',
-	testMatch: [ '<rootDir>/lib/**/*.spec.ts' ],
-	collectCoverage: true,
-	collectCoverageFrom: [ '<rootDir>/lib/**' ],
-	coverageReporters: [ 'lcov', 'text', 'html' ],
-	extensionsToTreatAsEsm: ['.ts'],
-};
